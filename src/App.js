5ff7cf04e676f98dd180fbcebac9f91f3d53a13a
Jean Alves <j_alves_19@hotmail.com>
2022-10-24 16:31:06 -0400

    criando o componente Title e renderizando um h2 com a prop headline

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import Header from './components/Header';
 import SolarSystem from './components/SolarSystem';
+import Title from './components/Title';
 
 class App extends Component {
   render() {
@@ -9,6 +10,7 @@ class App extends Component {
       <>
         <Header />
         <SolarSystem />
+        <Title />
       </>
     );
   }
