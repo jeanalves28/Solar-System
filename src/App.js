f0781fd67b00c03ff92b4fb055b6546de1a80112
Jean Alves <j_alves_19@hotmail.com>
2022-10-24 14:22:04 -0400

    criando o componente SolarSystem com uma id e renderizando no App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
 import Header from './components/Header';
+import SolarSystem from './components/SolarSystem';
 
 class App extends Component {
   render() {
-    return <Header />;
+    return (
+      <>
+        <Header />
+        <SolarSystem />
+      </>
+    );
   }
 }
 
