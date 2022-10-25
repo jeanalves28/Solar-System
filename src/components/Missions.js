e0099e2975f088656f9e5fe7b403b6910430f1cd
Jean Alves <j_alves_19@hotmail.com>
2022-10-24 23:35:35 -0400

    adicionando o componente MissionCard ao componente Missions e renderizando a lista missions

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,13 +1,27 @@
 import React, { Component } from 'react';
 import Title from './Title';
+import MissionCard from './MissionCard';
+import missions from '../data/missions';
 
 class Missions extends Component {
   render() {
     return (
-      <>
-        <div data-testid="missions" />
+      <div data-testid="missions">
         <Title headline="Missões" />
-      </>
+        {missions.map((objMission) => {
+          const { name, year, country, destination } = objMission;
+
+          return (
+            <MissionCard
+              key={ name }
+              name={ name }
+              year={ year }
+              country={ country }
+              destination={ destination }
+            />
+          );
+        })}
+      </div>
     );
   }
 }
