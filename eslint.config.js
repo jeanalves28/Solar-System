eb1e28976d730719828ada862af9fd4148a990b5
Jean Alves <j_alves_19@hotmail.com>
2026-07-13 19:13:31 -0400

    implementando o stylelint e o eslint para versão atual

--- eslint.config.js
+++ eslint.config.js
@@ -1,21 +1,32 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import js from '@eslint/js';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import testingLibrary from 'eslint-plugin-testing-library';
+import { defineConfig, globalIgnores } from 'eslint/config';
+import globals from 'globals';
 
 export default defineConfig([
-  globalIgnores(['dist']),
-  {
-    files: ['**/*.{js,jsx}'],
-    extends: [
-      js.configs.recommended,
-      reactHooks.configs.flat.recommended,
-      reactRefresh.configs.vite,
-    ],
-    languageOptions: {
-      globals: globals.browser,
-      parserOptions: { ecmaFeatures: { jsx: true } },
-    },
-  },
-])
+	globalIgnores(['dist']),
+	testingLibrary.configs['flat/react'],
+	{
+		files: ['**/*.{js,jsx}'],
+		extends: [
+			js.configs.recommended,
+			reactHooks.configs.flat.recommended,
+			reactRefresh.configs.vite,
+		],
+		languageOptions: {
+			globals: {
+				...globals.browser,
+				...globals.jest,
+			},
+			parserOptions: { ecmaFeatures: { jsx: true } },
+		},
+	},
+
+	{
+		rules: {
+			...testingLibrary.configs.react.rules,
+		},
+	},
+]);
--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,18 @@ import react, { reactCompilerPreset } from '@vitejs/plugin-react';
 import babel from '@rolldown/plugin-babel';
 
 export default defineConfig({
-	plugins: [react(), babel({ presets: [reactCompilerPreset()] })],
+	plugins: [
+		react(),
+		babel({ presets: [reactCompilerPreset(), '@babel/preset-react'] }),
+	],
 	base: '/Solar-System/',
 	server: {
 		port: 3000,
+	},
+	test: {
+		globals: true,
+		environment: 'jsdom',
+		setupFiles: './vitest.setup.js',
+		clearScreen: true,
 	},
 });
